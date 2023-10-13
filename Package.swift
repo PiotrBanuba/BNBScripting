95813d95ca93a6b5bd5a9218974691f22265afcf
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 10:32:01 +0300

    1.8.0-13

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-11"
+let bnbPackageVersion: Version = "1.8.0-13"
 
 let package = Package(
     name: "BNBScripting",
@@ -12,7 +12,7 @@ let package = Package(
         .library(
             name: "BNBScripting",
             targets: [
-                "BNBScripting"
+                "BNBScripting_Target"
             ]
         ),
     ],
@@ -21,7 +21,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "BNBScripting",
+            name: "BNBScripting_Target",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBScripting.zip",
             checksum: "ee70c769e66b8935e01cacad271771d1987e04d118dabc1c013f7d97defe0d3e"
         ),
