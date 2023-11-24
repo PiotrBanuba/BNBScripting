fa9e7d1bc51e6ddda95807b05e1f6b950d8714a1
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-11-24 11:49:18 +0300

    1.9.1-83-gf89cbbd807

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-64-g951557ebd1"
+let bnbPackageVersion: Version = "1.9.1-83-gf89cbbd807"
 
 let package = Package(
     name: "BNBScripting",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBScripting",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-64-g951557ebd1/BNBScripting.zip",
-            checksum: "d82553a1c3746765915421b774b550b908953cc9cebbc7b28b8010eef4150e48"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-83-gf89cbbd807/BNBScripting.zip",
+            checksum: "76f4026fef7a01143e8ab04d15fc8766227135691685dd985388fdfbc3b75705"
         ),
     ]
 )
